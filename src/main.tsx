1ed5ef55e998336ef47085000ab90aaed064869f
Rakshit <nazariya3330@gmail.com>
2025-09-23 01:23:16 +0530

    Initialize mock server and database

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,32 @@
-import { StrictMode } from "react";
-
-
-
-import { createRoot } from "react-dom/client";
-
-
 import "./index.css";
-
-
+import React from "react";
 import App from "./App.tsx";
-
-
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { initializeJobs } from "./services/db/jobsDb";
+import { initializeCandidates } from "./services/db/candidatesDb";
+import { initializeAssessments } from "./services/db/assessmentsDb";
 
+if (import.meta.env.MODE === "development") {
+  import("./services/mocks/browser").then(({ worker }) => {
+    worker
+      .start({
+        onUnhandledRequest: "warn",
+      })
+      .then(() => {
+        console.log("MSW started");
+      });
+  });
+}
 
-
-
+initializeJobs();
+initializeCandidates();
+initializeAssessments();
 
 createRoot(document.getElementById("root")!).render(
-
-
-  <StrictMode>
-
-
+  <React.StrictMode>
     <BrowserRouter>
-
-
       <App />
-
-
     </BrowserRouter>
-
-
-  </StrictMode>
-
-
+  </React.StrictMode>
 );
